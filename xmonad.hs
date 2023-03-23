4418d86fa1aa6131df3001e9dc32f7f25356aa41
Adrian Hieber <adrian.hieber@gmail.com>
2023-03-23 12:43:37 +0100

    changed terminal

--- xmonad.hs
+++ xmonad.hs
@@ -22,7 +22,8 @@ import System.IO
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal      = "gnome-terminal"
+--myTerminal      = "gnome-terminal"
+myTerminal      = "tilix"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
